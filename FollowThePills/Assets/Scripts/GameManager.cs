8494c00a12c776974c1bbe2f9cd32fcf3681849a
LordPet <opetit@hotmail.fr>
2018-03-10 22:29:21 -0500

    +script mouvement cam et perso, script collision et scirpt de collection de pilules, sscript de gameManager pour mettre le nombre de pilules à l'écran, ajout d'un premier path et ajout du personnage non animé,

---
+++ FollowThePills/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public int currentPills;
+	public Text pillsText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void AddPills(int pillsToAdd){
+		
+		currentPills += pillsToAdd;
+		pillsText.text = "Pilules: " + currentPills;
+	
+	}
+}
---
+++ FollowThePills/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour {
+
+	public int maxHealth;
+	public int currentHealth;
+
+	// Use this for initialization
+	void Start () {
+		currentHealth = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void HurtPlayer(int damage){
+
+		currentHealth -= damage;
+	}
+}
